c11386206ac85306fec88f09009e93cba310fca3
David Montero Crespo <davidmonterocrespo24@gmail.com>
2023-10-04 13:44:03 -0300

    Update __init__.py

--- micro_saas/models/__init__.py
+++ micro_saas/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
+from . import docker_tags
 from . import odoo_docker_instance
 from . import repository_repo
 from . import docker_compose_template
-from . import docker_tags
+
